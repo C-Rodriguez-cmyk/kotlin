f34e23558764d24ac1585bc55c1f34bc777014bc
C-Rodriguez-cmy <rc.caleb18@gmail.com>
2025-03-24 11:06:08 -0600

    segundo commit

---
+++ src/1_tipos_de_variables/Ejemplos.kt
@@ -0,0 +1,35 @@
+package `1_tipos_de_variables`
+
+fun main() {
+    // todas las variables son inmutables
+
+    // Variable inmutable de tipo entero
+    val edad: Int
+    edad = 48
+    // variable de tipo decimal
+    val sueldo: Float
+    sueldo = 1200.55f // siempre lleva una "f" al final del valor
+    // Variable inmutable de tipo decimal
+    val total: Double
+    total = 70000.24
+    // variable inmutable de tipo alfanumerico
+    val titulo: String
+    titulo = "Sistema de Ventas"
+    // variable mutable, las variables mutables empiezan con "var"
+    var mes: Int
+    mes = 1
+    // añgunas lineas mas de codigo
+    mes = 2
+    // variable de tipo char, almacena un unico caracter
+    var continua: Char = "s"
+    //Variable de tipo Boolean, True o false
+    var fin: Boolean = false
+
+    print("Caleb")
+    print("Samuel")
+
+    println("caleb")
+    println("samuel")
+
+    println(mes)
+}
---
+++ src/1_tipos_de_variables/proyecto2.kt
@@ -0,0 +1,20 @@
+package `1_tipos_de_variables`
+
+fun main(){
+    /*
+    Crear un programa que defina dos variables inmutables de tipo Int. Luego definir una
+ tercer variable mutable que almacene la suma de las dos primeras variables y las
+ muestre. Seguidamente almacenar en la variable el producto de las dos primeras
+ variables y mostrar el resultado.
+     */
+
+    val suma:Int
+    suma = 4
+    val suma:Float
+    suma= 9
+
+    val producto:Double
+    suma = 13
+
+    print("el resultado es 13")
+}
