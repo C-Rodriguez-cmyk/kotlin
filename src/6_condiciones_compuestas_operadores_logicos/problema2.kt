bd394532b84c9e06de6cdf77af25b162add1fb58
C-Rodríguez-cmy <rc.caleb18@gmail.com>
2025-04-21 09:26:58 -0600

    detalles

--- src/6_condiciones_compuestas_operadores_logicos/problema2.kt
+++ src/6_condiciones_compuestas_operadores_logicos/problema2.kt
@@ -3,17 +3,17 @@ package `6_condiciones_compuestas_operadores_logicos`
 /*
 Se ingresan tres valores por teclado, si todos son iguales calcular el cubo del número y mostrarlo.
  */
-
+import kotlin.math.pow
 fun main() {
-    print("ingrese primer valor: ")
-    val num1: Int = readln().toInt()
-    print("ingrese segundo valor: ")
-    val num2: Int = readln().toInt()
-    print("ingrese tercer valor: ")
-    val num3: Int = readln().toInt()
-    val valorcubo = (num1 * num2 * num3)
-    if (num1 == num2 && num2 == num3 && num1 == num3)
-        print("el valor del cubo es: $valorcubo")
-    else
-        print("los valores ingresados no son correctos para calcular el valor del cubo")
+    print("Ingrese primer valor: ")
+    val num1 = readln().toInt()
+    print("Ingrese segundo valor: ")
+    val num2 = readln().toInt()
+    print("Ingrese tercer valor: ")
+    val num3 = readln().toInt()
+
+    if (num1 == num2 && num2 == num3) {
+        val resultado = num1.toDouble().pow(3)
+        println("Todos los números son iguales")
+    }
 }
