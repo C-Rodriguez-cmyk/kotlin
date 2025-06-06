fe42953028fe5f83831711083c8e072fcf932859
C-Rodriguez-cmyk <rc.caleb18@gmail.com>
2025-06-06 10:24:24 -0600

    cambios

--- src/proyecto/ProyectoKotlin.kt
+++ src/proyecto/ProyectoKotlin.kt
@@ -1,38 +1,43 @@
 package proyecto
-fun registrarNota(nombre: String): Int {
-    var nota: Int?
+
+fun registrarNota(): Int {
+    var nota: Int
     do {
-        println("Ingrese la nota de $nombre (0-100): ")
+        println("Ingrese la nota del estudiante (0-100): ")
         nota = readln().toInt()
     } while (nota == null || nota < 0 || nota > 100)
     return nota
 }
 fun main() {
-    val estudiantes = arrayOf("Brithan", "Dilan", "Lawrence", "Angel", "Carlos")
-    val notas = Array(estudiantes.size) { i -> registrarNota(estudiantes[i]) }
+    val estudiantes = arrayOfNulls<String>(5)
+    val notas = IntArray(5)
     var aprobados = 0
     var reprobados = 0
     var sumaNotas = 0
     var NA = 0
     var NB = 100
-    for (nota in notas) {
-        sumaNotas += nota
-        if (nota >= 70)
+    for (i in 0 .. 4) {
+        println("ingrese el nombre del estudiante:")
+        val estudiante = readln().toString()
+        estudiantes[i] = estudiante
+        notas[i] = registrarNota()
+        sumaNotas += notas[i]
+        if (notas[i] >= 70)
             aprobados++
         else
             reprobados++
-        if (nota > NA)
-            NA = nota
-        if (nota < NB)
-            NB = nota
-        when (nota) {
-            in 90..100 -> println("estudiante: Excelente")
-            in 80..89 -> println("estudiante: Buena")
-            in 70..79 -> println("estudiante: Aprobada")
-            else -> println("estudiante: Reprobada")
+        if (notas[i] > NA)
+            NA = notas[i]
+        if (notas[i] < NB)
+            NB = notas[i]
+        when (notas[i]) {
+            in 90..100 -> println("${estudiantes[i]}: Excelente")
+            in 80..89 -> println("${estudiantes[i]}: Buena")
+            in 70..79 -> println("${estudiantes[i]}: Aprobada")
+            else -> println("${estudiantes[i]}: Reprobada")
         }
     }
-    val promedio = sumaNotas / notas.size
+    val promedio = sumaNotas / 5
     println("Estudiantes aprobados: $aprobados")
     println("Estudiantes reprobados: $reprobados")
     println("Promedio general de notas: $promedio")
